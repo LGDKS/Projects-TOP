67ad505d7b8c029fc8bafb5c1e254e280d85ae59
LGDKS <gonsenical@gmail.com>
2021-11-13 08:45:05 +0100

    Add playRound

--- project-rock-paper-scissors/js/script.js
+++ project-rock-paper-scissors/js/script.js
@@ -7,10 +7,12 @@ function computerPlay() {
         return 'Paper';
     } else if (computerThrow == 3) {
         return 'Scissors';
-    } else { return 'Error: Computer doesn\'t wanna play anymore'; }
+    } else {
+        return 'Error: Computer doesn\'t wanna play anymore';
+    }
 }
 
-let playerSelection = function () {
+let playerPlay = function () {
     let choice = prompt('Choose Rock, Paper or Scissors');
     let output = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
 
@@ -20,10 +22,31 @@ let playerSelection = function () {
         return 'Paper';
     } else if (output == 'Scissors') {
         return 'Scissors';
-    } else { return 'Error: Computer doesn\'t wanna play anymore'; }
-    return output;
+    } else {
+        return 'Error: Player made mistake';
+    }
+    
 }
 
+// scissors beat paper, paper beats rock, rock beats scissors,  
 
-let computerSelection = computerPlay();
+function playRound(computerSelection, playerSelection) {
+    computerSelection = computerPlay();
+    // console.log(computerSelection);
+    playerSelection = playerPlay();
+    // console.log(playerSelection);
 
+    if (computerSelection === playerSelection) {
+        return 'Draw!';
+    } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+        return 'You lose! Scissors beat Rock';
+    } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+        return 'You lose! Paper beats Rock';
+    } else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
+        return 'You lose! Rock beats Scissors';
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection} `;
+    }
+}
+
+console.log(playRound());
